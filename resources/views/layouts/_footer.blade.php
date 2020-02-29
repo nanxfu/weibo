b13cd8370dd5175c0793718a531bca050184bdf0
vagrant <vagrant@homestead>
2020-02-29 11:18:24 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
+    <a href="https://learnku.com/laravel/courses" _target = _blank></a>
+    <div class="float-right">
+    <a href="/about">关于</a>
+    </div>
+</footer>
