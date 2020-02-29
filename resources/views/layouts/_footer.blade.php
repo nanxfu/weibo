43793c3e081f76867eb19247193453d2087715a3
vagrant <vagrant@homestead>
2020-02-29 11:22:08 +0000

    命名路由器

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
     <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
     <a href="https://learnku.com/laravel/courses" _target = _blank></a>
     <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{route('about')}}">关于</a>
     </div>
 </footer>
