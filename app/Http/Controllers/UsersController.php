ccebbb5ae4faccadcb9d6b4b1a193b2baf5f31cf
nanxfu <791219591@qq.com>
2020-03-04 12:01:41 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,4 +15,16 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request){
+        $this->validate($request,
+        [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|ma:255',
+            'password' => 'required|confirmed|min:6'
+        ]
+        );
+
+        return;
+    }
 }
