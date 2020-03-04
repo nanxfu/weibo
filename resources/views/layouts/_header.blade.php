287dce425db1e0a4356dd5414f746ab3a2112adb
vagrant <vagrant@homestead>
2020-03-04 11:52:47 +0000

    用户注册表单3

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class = "navbar navbar-expand-lg navbar-dark bg-dark">
     <div class = "container">
-    <a href="navbar-brand" href = "{{route('home')}}">weibo APP</a>
+    <a class="navbar-brand" href = "{{route('home')}}">weibo APP</a>
     <ul class="navbar-nav justify-content-end">
         <li class="nav-item"><a href="{{route('help')}}" class="nav-link">帮助</a></li>
         <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
